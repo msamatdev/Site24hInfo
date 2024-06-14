b04ebbd717a8dbfa02982a103be35557a6b0e41d
NoWay_y <teolem77@gmail.com>
2024-06-14 18:43:36 +0200

    Fix lang button

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 
 document.getElementById("lang").addEventListener("click", function() {
-    let lang = window.location.href.split("/")[3];
+    let lang = window.location.href.split("/")[4];
     let newLang = lang === "fr" ? "en" : "fr";
     let newUrl = window.location.href.replace(lang, newLang);
     window.location.href = newUrl;
