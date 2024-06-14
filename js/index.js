308fb2a3a7930d6121bcf18595f91000169f0223
NoWay_y <teolem77@gmail.com>
2024-06-14 18:58:27 +0200

    Debug

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 document.getElementById("lang").addEventListener("click", function() {
     let lang = window.location.href.split("/")[4];
+    console.log(lang);
     let newLang = lang === "fr" ? "en" : "fr";
     let newUrl = window.location.href.replace(lang, newLang);
     window.location.href = newUrl;
